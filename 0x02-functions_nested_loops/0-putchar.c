62bcb9fcfec93f094de697149f10f70cdccd65b8
saheed <hgolaide@gmail.com>
2022-09-14 16:31:55 -0700

    Task 0. 0. _putchar. a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-  int n = 0;
-  char str_ch[8] = "_putchar";
+int n = 0;
+char str_ch[8] = "_putchar";
 
-  while (n < 8)
-    {
-      _putchar(str_ch[n]);
-      n += 1;
-    }
+while (n < 8)
+{
+_putchar(str_ch[n]);
+n += 1;
+}
   _putchar('\n');
   return (0);
 }
