063535d69f79e9fae6b695288a2cecf6ea996929
saheed <hgolaide@gmail.com>
2022-09-22 00:10:30 -0700

    Task 8. c function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *rot13(char *);
+char *infinte_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
