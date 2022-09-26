443c86453df87ba85f197aad15d346476656ee23
saheed <hgolaide@gmail.com>
2022-09-26 14:59:08 -0700

    Task 0. c function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,14 +13,14 @@ char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i = 0;
 
-for (i =0; i < n; i++)
-
+while (i < n)
 {
 
-*(s+i) = b; 
+*(s + i) = b;
+i += 1;
 
 }
-
+ 
 return (s);
 
 }
