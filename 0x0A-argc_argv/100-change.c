d1a6707dc5dbc5edd14e63f08dfd758f2387d6b4
saheed <hgolaide@gmail.com>
2022-10-01 11:35:49 -0700

    Task 5. c program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
-* main - program that prints minimum number of coins to make 
-*        change for an amount of money
+* main - program that prints minimum number of coins to make
+* change for an amount of money
 * @argc: argument count
 * @argv: argunment vector
 *
@@ -12,33 +12,38 @@
 
 int main(int argc, char *argv[])
 {
-int amount, cents;
+int amount, coins;
 if (argc != 2)
 {
 printf("Error\n");
-return(1);
+return (1);
 }
 amount = atoi(argv[1]);
-cents = 0;
-if(amount > 25)
-{while (amount >= 25)
-amount -= 25, cents++;
+coins = 0;
+if (amount > 25)
+{
+while (amount >= 25)
+amount -= 25, coins++;
 }
-if(amount > 10 && amount < 25)
-{while (amount >= 10)
-amount -= 10, cents++;
+if (amount > 10 && amount < 25)
+{
+while (amount >= 10)
+amount -= 10, coins++;
 }
-if(amount > 5 && amount < 10)
-{while (amount >= 5)
-amount -= 5, cents++;
+if (amount > 5 && amount < 10)
+{
+while (amount >= 5)
+amount -= 5, coins++;
 }
-if(amount > 2 && amount < 5)
-{while (amount >= 2)
-amount -= 2, cents++;
+if (amount > 2 && amount < 5)
+{
+while (amount >= 2)
+amount -= 2, coins++;
 }
-if(amount == 1 || amount == 2 || amount == 5 || amount == 10 || amount == 25)
-{cents ++;
+if (amount == 1 || amount == 2 || amount == 5 || amount == 10 || amount == 25)
+{
+coins++;
 }
-printf("%d\n", cents);
+printf("%d\n", coins);
 return (0);
 }
