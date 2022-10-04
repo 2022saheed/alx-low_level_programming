64d074986cc3eb467b98779827464d64d3286650
saheed <hgolaide@gmail.com>
2022-10-04 07:25:44 -0700

    Task 5 c. function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,46 +2,51 @@
 #include <stdlib.h>
 
 /**
- * *argstostr - concatenates all arguements to the program
- * @ac: arguement count
- * @av: pointer to arguements
- * Return: pointer to new space in memory or null
- **/
+* *argstostr - concatenates all arguements to the program
+* @ac: arguement count
+* @av: pointer to arguements
+* Return: pointer to new space in memory or null
+**/
 char *argstostr(int ac, char **av)
 {
-  char *strDup;
-  int i, j, k, size;
+char *strDup;
+int i, j, k, size;
 
-  if (ac == 0 || av == NULL)
-    return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-  size = 0;
-  /* count the number of chars in each string */
-  for (i = 0; i < ac; i++)
-    {
-      for (j = 0; av[i][j] != '\0'; j++)
-	size++;
-      size++;
-    }
-  size++;
+size = 0;
+/**
+* count the number of chars in each string
+*/
 
-  /* allocate memory for total number of chars and
-   * new line for each word
-   */
-  strDup = malloc(sizeof(char) * size);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+size++;
+size++;
+}
+size++;
 
-  if (strDup == NULL)
-    return (NULL);
+/**
+* allocate memory for total number of chars and
+* new line for each word
+*/
 
-  k = 0;
-  for (i = 0; i < ac; i++)
-    {
-      for (j = 0; av[i][j] != '\0'; j++)
-	{
-	  strDup[k++] = av[i][j];
-	}
-      strDup[k++] = '\n';
-    }
-  strDup[k] = '\0';
-  return (strDup);
+strDup = malloc(sizeof(char) * size);
+
+if (strDup == NULL)
+return (NULL);
+
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+strDup[k++] = av[i][j];
+}
+strDup[k++] = '\n';
+}
+strDup[k] = '\0';
+return (strDup);
 }
