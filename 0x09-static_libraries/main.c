cec38b04eee119071e756b7fe4b4632b33b2e14d
saheed <hgolaide@gmail.com>
2022-10-05 12:33:40 -0700

    Task 0. static library libmy.a containing all the functions listed

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,19 +1,7 @@
 #include "main.h"
-#include <stdio.h>
-  
-int main()
+
+int main(void)
 {
-  
-/**
-* Get the character to be written
-*/
-
-char ch = 'G';
-
-/**
-* Write the Character to stdout
-*/
-putchar(ch);
-  
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
