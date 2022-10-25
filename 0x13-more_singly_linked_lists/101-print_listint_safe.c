e0d198ef153b903e109280ce199011d28fc25821
saheed <hgolaide@gmail.com>
2022-10-25 05:56:19 -0700

    Task 12. c function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,24 @@
 #include "lists.h"
+
 /**
-* print_listint_safe - define function
-* @head:
-* Return: what does it return?
+* reverse_listint - prints a listint_t linked list.
+* @head: pointer to the list.
+* Return: number of nodes in the list.
 */
+
 size_t print_listint_safe(const listint_t *head)
 {
-head++;
-return (0);
+ size_t pichu = 0;
+const listint_t *aux_node = head;
+
+if (!head)
+exit(98);
+
+while (aux_node)
+{
+printf("[%p] %i\n", (void *)aux_node, aux_node->n);
+aux_node = aux_node->next;
+pichu++;
+}
+return (pichu);
 }
