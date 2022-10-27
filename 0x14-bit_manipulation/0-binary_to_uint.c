3986797a6fc9517dc32cb172445ed0146414eb48
saheed <hgolaide@gmail.com>
2022-10-27 06:28:35 -0700

    Task 0.0. c  function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -27,7 +27,7 @@ if (b[i] != '0' && b[i] != '1')
 return (0);
 for (j = len - 1; j > 0; j--)
 pow = pow * base;
-sum = sum + (pow * (b[i] - 48));
+sum = sum + (pow *(b[i] - 48));
 len--;
 pow = 1;
 }
