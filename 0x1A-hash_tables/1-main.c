8e953d41ce20926b88d1e293a0e2696e57fbac66
saheed <hgolaide@gmail.com>
2023-01-06 02:12:31 -0800

    1. djb2

--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -10,9 +10,7 @@
 */
 
 int main(void)
-  
 {
-  
 char *s;
 s = "cisfun";
 printf("%lu\n", hash_djb2((unsigned char *)s));
@@ -21,5 +19,4 @@ printf("%lu\n", hash_djb2((unsigned char *)s));
 s = "98";
 printf("%lu\n", hash_djb2((unsigned char *)s));
 return (EXIT_SUCCESS);
-  
 }
